7269a71e32d16ca8b2dc05ee1d9d84a7852a7b16
Gianijy <gianijyc.28@gmail.com>
2025-03-03 14:37:34 +0800

    Installed firebase dependencies and added firebase.js

---
+++ Back_End/firebase.js
@@ -0,0 +1,25 @@
+// firebase.js (Inside Back_End folder)
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // If using Firebase Auth
+import { getFirestore } from "firebase/firestore"; // If using Firestore
+import { getStorage } from "firebase/storage"; // If using Storage
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2CL9ZaJ4cJSo2vr8qizaC2FRPUlsHyiM",
+  authDomain: "criticheck.firebaseapp.com",
+  databaseURL: "https://criticheck-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "criticheck",
+  storageBucket: "criticheck.firebasestorage.app",
+  messagingSenderId: "529825237799",
+  appId: "1:529825237799:web:a942712c1504179aa57702",
+  measurementId: "G-KZ3JR7SVM0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
